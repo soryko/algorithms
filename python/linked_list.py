c86c3a81a54a074e63be382ee0d0717dab0f480f
soryko <sorykora@gmail.com>
2024-05-16 11:12:13 +0200

    added preprend function

--- python/linked_list.py
+++ python/linked_list.py
@@ -40,16 +40,23 @@ class LinkedList():
             self.tail = None
         else:
             tracker = self.head
-            incision_1 = tracker
+            incision = self.head
             while tracker.next != None:
-                incision_1 = tracker
+                incision = tracker
                 tracker = tracker.next    
-            incision_2 = tracker.next
-            self.tail = incision_1
+            self.tail = incision
             self.tail.next = None
-            incision_1.next = incision_2
             self.length -= 1
-        return incision_2
+        return incision
+    
+    def prepend(self, value):
+        if self.head == None:
+            self.append(value)
+        else:
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
 '''
 prepend function
 parameters :self, :value
@@ -96,5 +103,9 @@ linked_list.append(12)
 linked_list.append(38)
 linked_list.append(9)
 linked_list.append(662)
+linked_list.prepend(88)
+print("Head:", linked_list.head.value, "Memory:", linked_list.head)
+print("Tail:", linked_list.tail.value, "Memory:", linked_list.tail)
+print("Length:", linked_list.length, "Object:", vars(linked_list))
 
 linked_list.print_list()
