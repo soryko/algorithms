2da15e39d5b9d68eac8c61371e98265d68b5c871
soryko <sorykora@gmail.com>
2024-05-19 16:22:04 +0200

    append method for linked_list.rb

--- ruby/linked_list.rb
+++ ruby/linked_list.rb
@@ -1,65 +1,41 @@
 
 class LinkedList
-    attr_reader :list, :index, :i_range
+    attr_reader :head, :tail
     def initialize
-        @list = ""
-        @index = 0
-        @i_range = 19
+        @head = nil
+        @tail = nil
     end
-
-    def append(data)
-        if @index = 0 
-            node = HeadNode.new(data)
-            @list = node.figure
-        else 
-            unless @index == 1 do
-                value = @list[(-1 * @i_range + 8)..(-1 * @i_range + 10)].chomp
-                replacement_node = Node.n
-                list_copy = @list.duplicate[..@i_range]
-                list_copy += Node.figure
-            node = TailNode.new(data)
-            @list += node.figure
+    def print_list
+        nodes = []
+        current_node = @head
+        until current_node.nil?
+            nodes << current_node.value
+            current_node = current_node.next
+        end
+        puts nodes.join("] --> [")
+    end
+    def append_node(value)
+        new_node = Node.new(value)
+        if @head.nil? && @tail.nil?
+            @head = new_node
+            @tail = new_node
+        else
+            @tail.next = new_node
+            @tail = new_node
         end
     end
-
-    def prepend(data)
-        if list.length > 1
-            
 end
 
 class Node
-    attr_reader :head, :tail, :value, :next
-    def intialize(self, value)
+    attr_accessor :value, :next
+    def initialize(value)
         @value = value
-        @head = false
-        @tail = true
         @next = nil
-        @figure = "[NODE #{value} ] -->"
-    end
-
-    def next_node
-        nil
     end
 end
 
-class TailNode < Node
-    attr_reader :head, :tail, :value, :next
-    def initialize(self, value)
-        @value = value
-        @head = false
-        @tail = true
-        @next = nil
-        @figure = "[HNODE #{value}]-->"
-    end
-end
-
-class HeadNode < Node
-    attr_accessor :head, :tail, :value, :next
-    def initialize(self, value)
-        @value = value
-        @head = true
-        @tail = false
-        @next = nil
-        @figure = " [TNODE #{value} ] "
-    end
-end
+list = LinkedList.new
+list.append_node(5)
+list.append_node(15)
+list.append_node(41)
+list.print_list
