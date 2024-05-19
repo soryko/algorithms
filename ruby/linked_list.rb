2d869dbfdbabebe012dbb0b79d33a338b676b0da
soryko <sorykora@gmail.com>
2024-05-19 16:30:56 +0200

    added preprend() in .rb

--- ruby/linked_list.rb
+++ ruby/linked_list.rb
@@ -1,20 +1,28 @@
 
 class LinkedList
+    
     attr_reader :head, :tail
     def initialize
+
         @head = nil
         @tail = nil
     end
+
     def print_list
+
         nodes = []
+        start = "[head:"
+        last = ":tail]"
         current_node = @head
         until current_node.nil?
             nodes << current_node.value
             current_node = current_node.next
         end
-        puts nodes.join("] --> [")
+        puts start + nodes.join("] --> [") + last
     end
+
     def append_node(value)
+
         new_node = Node.new(value)
         if @head.nil? && @tail.nil?
             @head = new_node
@@ -22,6 +30,18 @@ class LinkedList
         else
             @tail.next = new_node
             @tail = new_node
+        end
+    end
+
+    def prepend(value)
+
+        new_node = Node.new(value)
+        if @head.nil? && @tail.nil?
+            @head = new_node
+            @tail = new_node
+        else
+            new_node.next = @head
+            @head = new_node
         end
     end
 end
@@ -38,4 +58,5 @@ list = LinkedList.new
 list.append_node(5)
 list.append_node(15)
 list.append_node(41)
+list.prepend(10)
 list.print_list
