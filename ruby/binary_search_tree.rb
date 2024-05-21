8c98361321643fdb8dce768295887325fb1aa3e0
soryko <sorykora@gmail.com>
2024-05-21 15:15:17 +0200

    added tree.insert

--- ruby/binary_search_tree.rb
+++ ruby/binary_search_tree.rb
@@ -50,8 +50,44 @@ class Tree
     build_tree(array[1..], root_node)
     root_node
   end
+
+  def insert(value)
+    if @root.nil?
+      @root = Node.new(value)
+      return
+    else
+      current_node = @root
+      while true
+        if current_node.value < value
+          previous_node = current_node
+          current_node = current_node.right
+        elsif current_node.value < value
+          previous_node = current_node
+          current_node = current_node.left
+        end
+        if value < current_node.right.value && value > current_node.left.value
+          next_node = current_node.right
+          current_node.right = Node.new(value)
+          current_node = current_node.right
+          current_node.right = next_node
+          break
+        elsif value > current_node.left.value && value < current_node.right.value
+          next_node = current_node.left
+          current_node.left = Node.new(value)
+          current_node = current_node.left
+          current_node.left = next_node
+          break
+        end
+      end
+      @root
+    end
+  end
 end
 
 array = [1,7,4,23,8,9,4,3,5,7,9,67,6345,324]
 tree = Tree.new(array)
+tree.pretty_print
+tree.insert(30)
+tree.insert(25)
+tree.insert(10)
 tree.pretty_print
